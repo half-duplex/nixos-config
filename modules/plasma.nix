3c4e86efd44b9170387155f1583354f540e6442c
Trevor Bergeron <mal@sec.gd>
2021-09-19 18:12:54 -0400

    add gwenview for plasma

--- modules/plasma.nix
+++ modules/plasma.nix
@@ -12,5 +12,9 @@ in
             displayManager.sddm.enable = true;
             desktopManager.plasma5.enable = true;
         };
+
+        environment.systemPackages = with pkgs; [
+            libsForQt5.gwenview
+        ];
     };
 }
