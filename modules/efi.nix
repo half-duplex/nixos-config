bc4c99278021b64d4f0ca5473fc1a7aecfe166eb
Trevor Bergeron <mal@sec.gd>
2021-09-29 19:28:19 -0400

    fix efi, add efibootmgr

--- modules/efi.nix
+++ modules/efi.nix
@@ -4,9 +4,14 @@
         type = lib.types.enum [ "efi" "bios" ];
         default = "efi";
     };
-    config = lib.mkIf (config.sconfig.boot == "bios") {
+
+    config = lib.mkIf (config.sconfig.boot == "efi") {
         boot.loader.efi.canTouchEfiVariables = true;
 
         security.tpm2.enable = true;
+
+        environment.systemPackages = with pkgs; [
+            efibootmgr
+        ];
     };
 }
