9fbcac6ec2baa53e7c2eba696552b7a920849415
Trevor Bergeron <mal@sec.gd>
2021-11-02 00:38:11 -0400

    oc: enable serial console

--- hosts/oc/configuration.nix
+++ hosts/oc/configuration.nix
@@ -5,6 +5,11 @@
         hardware = "qemu";
     };
 
+    boot = {
+        #initrd.kernelModules = [ "virtio_gpu" "drm" ];
+        kernelParams = [ "console=ttyAMA0" ];
+    };
+
     users.users.mal.openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEw0QgX7+nv9xsfHginV3pabQsoOIf96leLjglBfoQCk mal@awdbox.sec.gd"
     ];
