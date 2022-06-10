0304cbf6f936130a4d7aac66fb336d43e1584c07
Trevor Bergeron <mal@sec.gd>
2022-06-09 23:06:41 -0400

    remove alacritty unstable

--- modules/alacritty.nix
+++ modules/alacritty.nix
@@ -6,20 +6,6 @@ in
   options.sconfig.alacritty.enable = lib.mkEnableOption "Enable Alacritty";
 
   config = lib.mkIf cfg.enable {
-    #environment.systemPackages = [ pkgs.alacritty ];
-    environment.systemPackages = [
-      (pkgs.callPackage
-        (pkgs.fetchurl
-          {
-            url = "https://raw.githubusercontent.com/NixOS/nixpkgs/1ffba9f2f683063c2b14c9f4d12c55ad5f4ed887/pkgs/applications/terminal-emulators/alacritty/default.nix";
-            sha256 = "17fde99c1cba26662b06a7c0c8996e66d213d82879b0db18ca90156812c5d863";
-          }
-        )
-        {
-          inherit (pkgs.xorg) libXcursor libXxf86vm libXi;
-          inherit (pkgs.darwin.apple_sdk.frameworks) AppKit CoreGraphics CoreServices CoreText Foundation OpenGL;
-        }
-      )
-    ];
+    environment.systemPackages = [ pkgs.alacritty ];
   };
 }
