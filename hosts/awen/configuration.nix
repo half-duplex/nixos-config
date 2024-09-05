df3f8f3d7a02d3231bad1898910e42e21bec6215
mal <mal@sec.gd>
2024-09-05 19:59:03 -0400

    awen: postgres

--- hosts/awen/configuration.nix
+++ hosts/awen/configuration.nix
@@ -205,6 +205,15 @@ in
         };
       };
     };
+    postgresql = {
+      enable = true;
+      #package = pkgs.postgresql_15;
+      authentication = lib.mkOverride 10 ''
+        local all all peer
+        host all all 127.0.0.1/32 scram-sha-256
+        host all all ::1/128 scram-sha-256
+      '';
+    };
     samba = {
       enable = true;
       enableWinbindd = false;
