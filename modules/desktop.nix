37fe255d85bf070cdd2d1bc64337b41a33c7eade
mal <mal@sec.gd>
2024-11-24 21:01:51 -0500

    desktop: add iceman proxmark3

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -68,6 +68,7 @@ with lib;
       gnome-text-editor
       ghidra
       #pgadmin # ancient
+      (proxmark3.override { hardwarePlatform = "PM3GENERIC"; })
       remmina
       virt-manager
       wireshark
