4ce26e2c1a8c624dc07cd318a87aad786f8dbb08
mal <mal@sec.gd>
2025-01-28 12:12:17 -0500

    cli: set xdg user dirs

--- modules/cli.nix
+++ modules/cli.nix
@@ -88,6 +88,11 @@ in
       XDG_CACHE_HOME = "$HOME/.cache";
       XDG_CONFIG_HOME = "$HOME/.config";
       XDG_DATA_HOME = "$HOME/.local/share";
+      XDG_DESKTOP_DIR = "$HOME/desktop";
+      XDG_DOCUMENTS_DIR = "$HOME/library/documents";
+      XDG_DOWNLOAD_DIR = "$HOME/downloads";
+      XDG_MUSIC_DIR = "$HOME/library/music";
+      XDG_PICTURES_DIR = "$HOME/library/pictures";
       XDG_STATE_HOME = "$HOME/.local/state";
     };
     variables = {
