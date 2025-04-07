b1a0373ab2af5f6fb215b575f133b272c56c3936
mal <mal@sec.gd>
2025-04-06 21:08:24 -0400

    retain flake inputs

--- flake.nix
+++ flake.nix
@@ -48,6 +48,18 @@
         lanzaboote.nixosModules.lanzaboote
         lix-module.nixosModules.default
         sops-nix.nixosModules.sops
+        # Save inputs from GC
+        # https://github.com/NixOS/nix/issues/3995#issuecomment-2081164515
+        {
+          system.extraDependencies = let
+            collectFlakeInputs = input:
+              [input]
+              ++ builtins.concatMap collectFlakeInputs (
+                builtins.attrValues (input.inputs or {})
+              );
+          in
+            builtins.concatMap collectFlakeInputs (builtins.attrValues inputs);
+        }
       ];
     };
 }
