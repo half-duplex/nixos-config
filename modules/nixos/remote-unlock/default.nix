215866e1a010022ff70502d90d336791b618e369
mal <mal@sec.gd>
2025-05-19 22:35:14 -0400

    remote-unlock: update for systemd initrd

--- modules/nixos/remote-unlock/default.nix
+++ modules/nixos/remote-unlock/default.nix
@@ -2,24 +2,25 @@
   config,
   lib,
   namespace,
+  pkgs,
   ...
 }: let
 in {
   options.${namespace}.remoteUnlock.enable = lib.mkEnableOption "Configure remote-unlock for FDE";
 
   config = lib.mkIf config.${namespace}.remoteUnlock.enable {
-    boot.initrd.network = {
-      enable = true;
-      postCommands = ''
-        echo 'zfs load-key -ra;killall zfs;exit' >>/root/.profile
-      '';
-      ssh = {
+    boot.initrd = {
+      network = {
         enable = true;
-        authorizedKeys = config.users.users.mal.openssh.authorizedKeys.keys;
-        hostKeys = [
-          "/persist/ssh/ssh_host_ed25519_key_initrd"
-        ];
+        ssh = {
+          enable = true;
+          authorizedKeys = config.users.users.mal.openssh.authorizedKeys.keys;
+          hostKeys = [
+            "/persist/ssh/ssh_host_ed25519_key_initrd"
+          ];
+        };
       };
+      systemd.users.root.shell = "${pkgs.systemd}/bin/systemd-tty-ask-password-agent";
     };
     networking.interfaces.eth0.useDHCP = true;
   };
