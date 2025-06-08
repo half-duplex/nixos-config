04880c4a0332fd8cbbd17dda96ba9b1d0b8796ac
mal <mal@sec.gd>
2025-06-07 23:07:23 -0400

    baseline: enable erofs, required for etc overlay

--- modules/nixos/baseline/default.nix
+++ modules/nixos/baseline/default.nix
@@ -138,7 +138,6 @@ in {
       "decnet"
       "econet"
       "efs"
-      "erofs"
       "exofs"
       "freevxfs"
       "gfs2"
@@ -169,7 +168,7 @@ in {
     ];
   };
 
-  system.etc.overlay.enable = true;
+  #system.etc.overlay.enable = true;
 
   #environment.memoryAllocator.provider = "graphene-hardened"; # Breaks everything... ??
   #environment.memoryAllocator.provider = "scudo"; # Breaks firefox...
