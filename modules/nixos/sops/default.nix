263313ad6eac791ccecc21b0167d48f96d93fe4a
mal <mal@sec.gd>
2025-07-17 01:08:01 -0400

    sops: only use the correct ssh key path for a host

--- modules/nixos/sops/default.nix
+++ modules/nixos/sops/default.nix
@@ -4,11 +4,17 @@
   namespace,
   pkgs,
   ...
-}: {
+}: let
+  impermanent = config.${namespace}.impermanence.enable;
+in {
   sops = {
     age.sshKeyPaths = [
-      "/etc/ssh/ssh_host_ed25519_key"
-      "/persist/ssh/ssh_host_ed25519_key"
+      ((
+          if impermanent
+          then "/persist"
+          else "/etc"
+        )
+        + "/ssh/ssh_host_ed25519_key")
     ];
   };
   environment.systemPackages = with pkgs; [sops ssh-to-age];
