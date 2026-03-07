3dea96a4fefb9051af108a997df656d6c4bd0f9c
mal <mal@sec.gd>
2026-03-07 18:59:43 -0500

    tdesktop: fix build

--- modules/nixos/overlays/telegram-desktop/default.nix
+++ modules/nixos/overlays/telegram-desktop/default.nix
@@ -20,6 +20,11 @@
                   url = "https://github.com/Layerex/telegram-desktop-patches/raw/36e27074851c64e52706adc606d1a9bfc12a3194/0004-Disable-accounts-limit.patch";
                   hash = "sha256-PZWCFdGE/TTJ1auG1JXNpnTUko2rCWla6dYKaQNzreg=";
                 })
+                # https://github.com/NixOS/nixpkgs/issues/497549
+                (prev.pkgs.fetchpatch {
+                  url = "https://gist.github.com/half-duplex/d95e4fda535fb72ad0246ccfbe55cb23/raw/410dc924a317d391226c338ab75fcd1a9aaaf91b/tdesktop-minizip-include.patch";
+                  hash = "sha256-lvEE5ZGmOjulZCg/rgrvAOTjUpJsAOcga+sAzr8FtYA=";
+                })
               ];
             separateDebugInfo = true;
           };
