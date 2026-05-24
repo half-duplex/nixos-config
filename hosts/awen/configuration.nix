4f378cef1b1ace040fa426260ee3da80991e079e
mal <mal@sec.gd>
2026-05-24 01:19:41 -0400

    awen: frigate changes

--- hosts/awen/configuration.nix
+++ hosts/awen/configuration.nix
@@ -52,7 +52,7 @@ in {
       frigate = {
         enable = true;
         hostname = "panopticon.sec.gd";
-        secrets = ["cam_task" "cam_workshop" "cam_wyze"];
+        secrets = ["cam_task_pw" "cam_workshop_pw" "cam_wyze_pw"];
         useUSBCoral = true;
         settings = {
           mqtt = {
@@ -60,30 +60,87 @@ in {
             user = "frigate";
             password = "{FRIGATE_MQTT_PASSWORD}";
           };
+          face_recognition = {
+            enabled = true;
+            #model_size = "large";
+          };
+          motion.enabled = true;
+          record = {
+            # frigate 0.17
+            #continuous.days = 1;
+            #motion.days = 15;
+            retain = {
+              days = 5;
+              mode = "all";
+            };
+            detections.retain = {
+              days = 15;
+              mode = "motion";
+            };
+          };
           cameras = {
             task = {
               enabled = true;
-              live.stream_name = "task_hd";
+              record.enabled = true;
+              live.streams = {
+                "High Quality" = "task_hd";
+                "Low Quality" = "task";
+              };
+              onvif = {
+                host = "10.0.65.182";
+                port = 80;
+                user = "thingino";
+                password = "{FRIGATE_CAM_TASK_PW}";
+              };
               ffmpeg.inputs = [
                 {
+                  path = "rtsp://stream:{FRIGATE_GO2RTC_PASSWORD}@[::1]:8554/task?timeout=30";
+                  roles = ["detect"];
+                }
+                {
                   path = "rtsp://stream:{FRIGATE_GO2RTC_PASSWORD}@[::1]:8554/task_hd?timeout=30";
-                  roles = [];
+                  roles = ["record"];
                 }
               ];
             };
             workshop = {
               enabled = true;
-              live.stream_name = "workshop_hd";
+              record.enabled = true;
+              live.streams = {
+                "High Quality" = "workshop_hd";
+                "Low Quality" = "workshop";
+              };
+              onvif = {
+                host = "10.0.65.181";
+                port = 80;
+                user = "thingino";
+                password = "{FRIGATE_CAM_WORKSHOP_PW}";
+              };
               ffmpeg.inputs = [
                 {
+                  path = "rtsp://stream:{FRIGATE_GO2RTC_PASSWORD}@[::1]:8554/workshop?timeout=30";
+                  roles = ["detect"];
+                }
+                {
                   path = "rtsp://stream:{FRIGATE_GO2RTC_PASSWORD}@[::1]:8554/workshop_hd?timeout=30";
-                  roles = [];
+                  roles = ["record"];
                 }
               ];
             };
             wyze = {
               enabled = true;
-              live = {stream_name = "wyze_hd";};
+              detect.enabled = true;
+              record.enabled = true;
+              live.streams = {
+                "High Quality" = "wyze_hd";
+                "Low Quality" = "wyze";
+              };
+              onvif = {
+                host = "10.0.65.180";
+                port = 80;
+                user = "thingino";
+                password = "{FRIGATE_CAM_WYZE_PW}";
+              };
               ffmpeg.inputs = [
                 {
                   path = "rtsp://stream:{FRIGATE_GO2RTC_PASSWORD}@[::1]:8554/wyze?timeout=30";
@@ -94,18 +151,15 @@ in {
                   roles = ["record"];
                 }
               ];
-              detect.enabled = true;
-              record.enabled = true;
-              motion.enabled = true;
             };
           };
           go2rtc.streams = {
-            #task = "rtsp://\${cam_task}/sd#timeout=30";
-            task_hd = "rtsp://\${cam_task}/hd#timeout=30";
-            #workshop = "rtsp://\${cam_workshop}/sd#timeout=30";
-            workshop_hd = "rtsp://\${cam_workshop}/hd#timeout=30";
-            wyze = "rtsp://\${cam_wyze}/sd#timeout=30";
-            wyze_hd = "rtsp://\${cam_wyze}/hd#timeout=30";
+            task = "rtsp://thingino:\${cam_task_pw}@10.0.65.182/sd#timeout=30&backchannel=0";
+            task_hd = "rtsp://thingino:\${cam_task_pw}@10.0.65.182/hd#timeout=30&backchannel=0";
+            workshop = "rtsp://thingino:\${cam_workshop_pw}@10.0.65.181/sd#timeout=30&backchannel=0";
+            workshop_hd = "rtsp://thingino:\${cam_workshop_pw}@10.0.65.181/hd#timeout=30&backchannel=0";
+            wyze = "rtsp://thingino:\${cam_wyze_pw}@10.0.65.180/sd#timeout=30&backchannel=0";
+            wyze_hd = "rtsp://thingino:\${cam_wyze_pw}@10.0.65.180/hd#timeout=30&backchannel=0";
           };
         };
       };
