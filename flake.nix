1aa65437f6cbbb5cd27d6b18804a65d234ba91b1
mal <mal@sec.gd>
2026-09-11 23:24:10 -0400

    update

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     # system
-    nixpkgs.url = "nixpkgs/nixos-26.05";
+    nixpkgs.url = "https://channels.nixos.org/nixos-26.05/nixexprs.tar.zst";
     nixpkgsStaging.url = "nixpkgs/release-26.05";
     #nixpkgsUnstable.url = "nixpkgs/nixos-unstable";
     blank.url = "github:divnix/blank?rev=5a5d2684073d9f563072ed07c871d577a6c614a8";
