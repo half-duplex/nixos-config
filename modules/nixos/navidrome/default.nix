87da547ee912bca03fec6d42435d9fa414dbea68
mal <mal@sec.gd>
2026-09-15 16:30:46 -0400

    navidrome: more cache, more knobs

--- modules/nixos/navidrome/default.nix
+++ modules/nixos/navidrome/default.nix
@@ -22,6 +22,16 @@ in {
         type = lib.types.str;
       };
     };
+    dataDir = lib.mkOption {
+      default = "/persist/navidrome";
+      description = "Where to store application data and backups";
+      type = lib.types.path;
+    };
+    cacheDir = lib.mkOption {
+      default = "/persist/nobackup/navidrome";
+      description = "Directory for caches, like image and transcode";
+      type = lib.types.path;
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -31,20 +41,19 @@ in {
         environmentFile = config.sops.secrets."navidrome.env".path;
         settings = {
           BaseURL = "https://${cfg.nginx.hostname}";
-          CacheFolder = "/persist/nobackup/navidrome";
-          DataFolder = "/persist/navidrome";
+          CacheFolder = cfg.cacheDir;
+          DataFolder = cfg.dataDir;
           MusicFolder = "/mnt/data/library/music";
           AlbumPlayCountMode = "normalized";
           AutoImportPlaylists = false;
-          Backup.Path = "/persist/navidrome/backups";
+          Backup.Path = cfg.dataDir + "/backups";
           Backup.Schedule = "0 7 4 * *";
           Backup.Count = 12;
           DefaultShareExpiration = "720h"; # 30d
           EnableInsightsCollector = true;
-          ImageCacheSize = "100MiB";
-          PlaylistsPath = "../playlists";
-          #ReverseProxyWhitelist = "127.0.0.1";
-          TranscodingCacheSize = "500MiB";
+          ImageCacheSize = "512MiB";
+          PlaylistsPath = "playlists";
+          TranscodingCacheSize = "8192MiB";
         };
       };
       nginx.virtualHosts = lib.mkIf cfg.nginx.enable {
@@ -66,7 +75,10 @@ in {
         };
       };
     };
-    systemd.services.navidrome.serviceConfig.BindReadOnlyPaths = "/mnt/data/downloads";
+    systemd.services.navidrome.serviceConfig.BindReadOnlyPaths = [
+      "/mnt/data/downloads"
+      "/mnt/data/library/playlists"
+    ];
     sops.secrets."navidrome.env" = {
       restartUnits = ["navidrome.service"];
       sopsFile = secrets/${config.networking.hostName}.yaml;
